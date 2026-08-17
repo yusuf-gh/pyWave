68a5acf038b7182e6d15e29e7716d2d9f9c6cf0c
Little <146277525+yusuf-gh@users.noreply.github.com>
2026-08-17 22:04:44 +0500

    made a loop for socket connection

--- src/pywave/server.py
+++ src/pywave/server.py
@@ -1,5 +1,4 @@
 import socket
-from multiprocessing.connection import Client
 
 connection = socket.socket()
 host = "0.0.0.0"
@@ -8,7 +7,24 @@ port = 8000
 connection.bind((host, port))
 connection.listen(5)
 
+connection.settimeout(15)
+
+
 print(f"Listening on {host}:{port}")
 
-client_socket, client_address = connection.accept()
-print(f"New connection from {client_address}")
+while True:
+    try:
+        client_socket, client_address = connection.accept()
+        print(f"\n!!! NEW CONNECTION FROM !!!\n {client_address}\n\n")
+        request = client_socket.recv(1024)
+        print(request.decode())
+        response = b"HTTP/1.1 200 OK\r\n\r\nHello, world!"
+        client_socket.send(response)
+    except socket.timeout:
+        print(f"\n!!! TIMEOUT !!!\n")
+        client_socket.close()
+        break
+
+
+
+
